44d373171088b0e8a15ee276d8ff936ba6e241ea
mad547 <97340914+mad547@users.noreply.github.com>
2026-07-04 17:04:34 +0600

    зарегестрировал модели в админке с profileinline

--- source/accounts/admin.py
+++ source/accounts/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+
+from accounts.models import Profile
+
 
 # Register your models here.
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    fields = ['avatar', 'messages_count']
+
+
+class ProfileAdmin(UserAdmin):
+    inlines = [ProfileInline]
+
+
+user = get_user_model()
+admin.site.unregister(User)
+admin.site.register(User, ProfileAdmin)
+admin.site.register(Profile)
--- source/threads/admin.py
+++ source/threads/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
+import threads
+from threads.models import Thread, Answer
+
+
 # Register your models here.
+class AnswerInline(admin.TabularInline):
+    model = Answer
+    extra = 1
+
+
+class ThreadAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'created_at', 'answers_count']
+    search_fields = ['title', 'content']
+    inlines = [AnswerInline]
+
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ['author','thread', 'created_at']
+    search_fields = ['content']
+
+
+admin.site.register(Thread, ThreadAdmin)
+admin.site.register(Answer, AnswerAdmin)
