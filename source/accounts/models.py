a0fef1e2c284785317842d6367345ee2b551d042
mad547 <97340914+mad547@users.noreply.github.com>
2026-07-04 13:44:07 +0600

    Добавил модели profile thread answer

--- source/accounts/models.py
+++ source/accounts/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
+
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='profile',
+        verbose_name='Пользователь'
+    )
+    avatar = models.ImageField(
+        upload_to='avatars',
+        null=False,
+        blank=False,
+        verbose_name='Аватар'
+    )
+    messages_count = models.PositiveIntegerField(
+        default=0,
+        verbose_name='Количество сообщений'
+    )
+
+    def __str__(self):
+        return f'Профиль {self.user.username}'
+
+    class Meta:
+        db_table = 'profile'
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
--- source/threads/models.py
+++ source/threads/models.py
@@ -1,3 +1,74 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.db.models.fields import PositiveIntegerField
+
 
 # Create your models here.
+class Thread(models.Model):
+    title = models.CharField(
+        max_length=300,
+        null=False,
+        blank=False,
+        verbose_name='Название'
+    )
+    content = models.TextField(
+        null=False,
+        blank=False,
+        verbose_name='Содержимое'
+    )
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='threads',
+        verbose_name='Автор'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата создания'
+    )
+    answers_count = PositiveIntegerField(
+        default=0,
+        verbose_name='Количество ответов'
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = 'thread'
+        verbose_name = 'Тема'
+        verbose_name_plural = 'Темы'
+        ordering = ['-created_at']
+
+
+class Answer(models.Model):
+    thread = models.ForeignKey(
+        Thread,
+        on_delete=models.CASCADE,
+        related_name='answers',
+        verbose_name='Тема'
+    )
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='answers',
+        verbose_name='Автор'
+    )
+    content = models.TextField(
+        null=False,
+        blank=False,
+        verbose_name='Текст'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата создания'
+    )
+
+    def __str__(self):
+        return f'Ответ {self.author.username} в {self.thread.title}'
+
+    class Meta:
+        db_table = 'answer'
+        verbose_name = 'Ответ'
+        verbose_name_plural = 'Ответы'
+        ordering = ['-created_at']
