49c3b9300561eb21b93438542a412f6e886647b1
mad547 <97340914+mad547@users.noreply.github.com>
2026-07-04 15:49:40 +0600

    добавил views для аккаунтов б тем и ответов с проверкой прав

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.contrib.auth import login, get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DetailView
+
+from accounts.forms import RegisterForm
+from accounts.models import Profile
+
 
 # Create your views here.
+class UserLoginView(LoginView):
+    template_name = 'accounts/login.html'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('index')
+
+
+class UserLogoutView(LogoutView):
+    next_page = 'index'
+
+
+class RegisterView(CreateView):
+    model = get_user_model()
+    template_name = 'accounts/register.html'
+    form_class = RegisterForm
+
+    def post(self, request, *args, **kwargs):
+        self.object = None
+        form = self.get_form()
+        if form.is_valid():
+            user = form.save()
+            Profile.objects.create(
+                user=user,
+                avatar=form.cleaned_data['avatar'],
+            )
+            login(request, user)
+            return redirect('index')
+        return self.render_to_response(self.get_context_data(form=form))
+
+
+class UserDetailView(LoginRequiredMixin, DetailView):
+    model = get_user_model()
+    template_name = 'accounts/profile.html'
+    context_object_name = 'profile_user'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['threads'] = self.get_object().threads.order_by('-created_at')
+        return context
--- source/threads/views.py
+++ source/threads/views.py
@@ -1,3 +1,135 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.db.models import Q
+from django.shortcuts import redirect, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+from threads.forms import ThreadForm, AnswerForm, SearchForm
+from threads.models import Thread, Answer
+
 
 # Create your views here.
+class IndexView(ListView):
+    template_name = 'threads/index.html'
+    context_object_name = 'threads'
+    paginate_by = 10
+
+    def get_queryset(self):
+        queryset = Thread.objects.all().order_by('-created_at')
+        query = self.request.GET.get('query', '')
+        if query:
+            queryset = queryset.filter(
+                Q(title__icontains=query) |
+                Q(content__icontains=query)
+            )
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['search_form'] = SearchForm(self.request.GET)
+        context['query'] = self.request.GET.get('query', '')
+        return context
+
+
+class ThreadDetailView(DetailView):
+    model = Thread
+    template_name = 'threads/thread_detail.html'
+    context_object_name = 'thread'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['answers'] = self.object.answers.order_by('created_at')
+        context['answer_form'] = AnswerForm()
+        return context
+
+
+class ThreadCreateView(LoginRequiredMixin, CreateView):
+    model = Thread
+    template_name = 'threads/thread_form.html'
+    form_class = ThreadForm
+
+    def form_valid(self, form):
+        thread = form.save(commit=False)
+        thread.author = self.request.user
+        thread.save()
+        return redirect('threads:thread_detail', pk=thread.pk)
+
+
+class ThreadUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Thread
+    template_name = 'threads/thread_form.html'
+    form_class = ThreadForm
+
+    def test_func(self):
+        thread = self.get_object()
+        return self.request.user == thread.author or \
+               self.request.user.has_perm('threads.change_thread')
+
+    def get_success_url(self):
+        return reverse_lazy('threads:thread_detail', kwargs={'pk': self.object.pk})
+
+
+class ThreadDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Thread
+    template_name = 'threads/thread_confirm_delete.html'
+    context_object_name = 'thread'
+    success_url = reverse_lazy('index')
+
+    def test_func(self):
+        thread = self.get_object()
+        return self.request.user == thread.author or \
+               self.request.user.has_perm('threads.delete_thread')
+
+
+class AnswerCreateView(LoginRequiredMixin, CreateView):
+    model = Answer
+    form_class = AnswerForm
+
+    def form_valid(self, form):
+        thread = get_object_or_404(Thread, pk=self.kwargs.get('pk'))
+        answer = form.save(commit=False)
+        answer.author = self.request.user
+        answer.thread = thread
+        answer.save()
+        thread.answers_count += 1
+        thread.save()
+        self.request.user.profile.messages_count += 1
+        self.request.user.profile.save()
+        return redirect('threads:thread_detail', pk=thread.pk)
+
+
+class AnswerUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Answer
+    template_name = 'threads/answer_form.html'
+    form_class = AnswerForm
+
+    def test_func(self):
+        answer = self.get_object()
+        return self.request.user == answer.author or \
+               self.request.user.has_perm('threads.change_answer')
+
+    def get_success_url(self):
+        return reverse_lazy('threads:thread_detail', kwargs={'pk': self.object.thread.pk})
+
+
+class AnswerDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Answer
+    template_name = 'threads/answer_confirm_delete.html'
+    context_object_name = 'answer'
+
+    def test_func(self):
+        answer = self.get_object()
+        return self.request.user == answer.author or \
+               self.request.user.has_perm('threads.delete_answer')
+
+    def form_valid(self, form):
+        answer = self.get_object()
+        thread = answer.thread
+        thread.answers_count -= 1
+        thread.save()
+        answer.author.profile.messages_count -= 1
+        answer.author.profile.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('threads:thread_detail', kwargs={'pk': self.object.thread.pk})
